387af04e455226be22b719820d2d75829b74e222
Joe Santos <jaysantos012@gmail.com>
2021-06-22 00:41:59 -0400

    adding the operator and the deciaml

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
 function sendNumberValue(number) {
-    calculatorDisplay.textContent = number;
+    //if current display value is 0, relapce it, if not add number 
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 };
 
+function addDecimal() {
+    // if no decimal , add one
+
+    if(!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+    }
+}
 // Add event listeners for number, operator, decimal buttons
 
 inputBtns.forEach((inputBtn) => {
@@ -14,6 +23,14 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click' || 'touchstart', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click' || 'touchstart', () => sendNumberValue());
+        inputBtn.addEventListener('click' || 'touchstart', () => addDecimal());
     }
 })
+// reset diplay
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+//clear event listener
+
+clearBtn.addEventListener('click' || 'touchstart', resetAll);
